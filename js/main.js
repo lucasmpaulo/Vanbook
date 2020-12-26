1bcd63b9c35206dbbfc3c6c122757e25866ed09d
Lucas Paulo <mesakilive@gmail.com>
2020-12-26 18:15:36 -0300

    Adicionado evento de scroll na seção beneficios

--- js/main.js
+++ js/main.js
@@ -40,3 +40,15 @@ $('ul.menu').menuMobile({
   background: '#FFFFFF',
   color: '#29383e'
 })
+
+// Animação ao ativar o scroll
+$(window).scroll(function() {
+  var windowTop = $(this).scrollTop();
+  $('.anime').each(function() {
+    if(windowTop > $(this).offset().top - 400) {
+      $(this).addClass('anime-init');
+    } else {
+      $(this).removeClass('anime-init');
+    }
+  });
+});
