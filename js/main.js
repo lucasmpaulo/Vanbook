e3a90a3f11a0e5f188abe3640edaf4d7d59501df
Lucas Paulo <mesakilive@gmail.com>
2020-12-28 11:12:47 -0300

    Alterando dimensão no efeito do scroll

--- js/main.js
+++ js/main.js
@@ -45,10 +45,11 @@ $('ul.menu').menuMobile({
 $(window).scroll(function() {
   var windowTop = $(this).scrollTop();
   $('.anime').each(function() {
-    if(windowTop > $(this).offset().top - 400) {
+    if(windowTop > $(this).offset().top - 500) {
       $(this).addClass('anime-init');
     } else {
       $(this).removeClass('anime-init');
     }
   });
 });
+
